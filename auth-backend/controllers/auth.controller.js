cf228550e2345bbb449b687a3e07b9ca02523b8d
Zaid12-u <zaidaziz139@gmail.com>
2026-05-25 02:38:43 +0500

    fix: brevo port 25

--- auth-backend/controllers/auth.controller.js
+++ auth-backend/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
+    port: 25,
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
